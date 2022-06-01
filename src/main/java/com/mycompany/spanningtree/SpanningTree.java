ba2075874e0898b568002b38e1efb7f40a4fad8c
Li595 <61666020+Li595@users.noreply.github.com>
2022-06-01 16:39:52 +0300

    Update SpanningTree.java

--- src/main/java/com/mycompany/spanningtree/SpanningTree.java
+++ src/main/java/com/mycompany/spanningtree/SpanningTree.java
@@ -1,23 +1,28 @@
 package com.mycompany.spanningtree;
 
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class SpanningTree {
         
     public static void main(String[] args){
+        List<Connections> connectionsList = new ArrayList<Connections>();
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("How do you want to input?(text/file): ");
         if(scanner.nextLine().equals("text"))
         {
-            Input.InputFromConsole();
+            //TODO соединить получаемый лист с консольным вводом
+            ConsoleInput connections = new ConsoleInput();
+            connections.InputFromConsole();
         }
         else
         {
             try
             {
-                Input.InputFromFile();               
+                FileInput connections = new FileInput();
+                connectionsList = connections.InputFromFile();
+                //connections.InputFromFile();
             }
             catch(FileNotFoundException e)
             {
@@ -25,12 +30,15 @@ public class SpanningTree {
                 System.out.println("Do you want to input from console?(y/n)");
                 if(scanner.nextLine().equals("y"))
                 {
-                    Input.InputFromConsole();
+                    ConsoleInput connections = new ConsoleInput();
+                    connections.InputFromConsole();
                 }
             }
             finally{
                 scanner.close();
             }
         }
+        Sorting ascendedConnections = new Sorting();
+        ascendedConnections.ascendingWeight(connectionsList);                         
     }
 }
