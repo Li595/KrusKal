d994d9b975115cb32888b055cabdfe523f965380
Li595 <61666020+Li595@users.noreply.github.com>
2022-06-02 20:09:44 +0300

    Родительский класс

---
+++ src/main/java/com/mycompany/spanningtree/Input.java
@@ -0,0 +1,9 @@
+package com.mycompany.spanningtree;
+
+public class Input {
+    
+    public static char getCharFromString(String str, int index)
+    {
+        return str.charAt(index);
+    } 
+}
