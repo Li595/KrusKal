b13053f45671020e19fa00db1a644a5b69b0b65e
Li595 <61666020+Li595@users.noreply.github.com>
2022-06-03 15:41:21 +0300

    Delete src/main/java/com/mycompany/spanningtree directory

--- src/main/java/com/mycompany/spanningtree/Input.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.spanningtree;
-
-public class Input {
-    
-    public static char getCharFromString(String str, int index)
-    {
-        return str.charAt(index);
-    } 
-}
